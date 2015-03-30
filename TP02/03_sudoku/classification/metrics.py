903df45396ab8bde1f27bea4cae8be8b56a9fc14
miiicmueller <michael.muller@he-arc.ch>
2015-03-30 12:03:38 +0200

    Ajout du .init

---
+++ TP02/03_sudoku/classification/metrics.py
@@ -0,0 +1,18 @@
+from sklearn.metrics.metrics import confusion_matrix, classification_report
+import pylab as pl
+
+
+def show_confusion_matrix(y_true, y_predicted, title=''):
+    """
+    Plot (and print) a confusion matrix from y_true and y_predicted
+    """
+
+    # TODO: show confusion matrix plot
+
+
+def print_classification_report(y_true, y_pred, title=''):
+    """
+    Print a classification report
+    """
+
+    # TODO: print classification report
